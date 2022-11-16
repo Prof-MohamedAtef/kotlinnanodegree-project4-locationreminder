d1b4a4876c7ddcccf4be80a9feae2eba52775d57
m_wallet_app <mwallet.apps@gmail.com>
2022-11-16 10:43:21 +0200

    added purchased apikey in local.properties file | added location permission

--- starter/app/src/main/java/com/udacity/project4/atef/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/atef/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,21 +1,38 @@
 package com.udacity.project4.atef.locationreminders.savereminder.selectreminderlocation
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Transformations.map
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.udacity.project4.atef.BuildConfig
+import com.udacity.project4.atef.BuildConfig.MAPS_API_KEY
+import com.udacity.project4.atef.BuildConfig.MAPS_API_KEY_PAID
 import com.udacity.project4.atef.R
 import com.udacity.project4.atef.base.BaseFragment
 import com.udacity.project4.atef.databinding.FragmentSelectLocationBinding
 import com.udacity.project4.atef.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.atef.utils.FOREGROUND_LOCATION_PERMISSIONS
+import com.udacity.project4.atef.utils.anyPermissionsGranted
+import com.udacity.project4.atef.utils.requestMissingPermissions
 import com.udacity.project4.atef.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 
-class SelectLocationFragment : BaseFragment() {
+class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
+
+    private var mMap: GoogleMap?=null
 
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
@@ -28,11 +45,18 @@ class SelectLocationFragment : BaseFragment() {
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
 
-//        TODO: add the map setup implementation
+        if (!anyPermissionsGranted(FOREGROUND_LOCATION_PERMISSIONS)) {
+            requestMissingPermissions(FOREGROUND_LOCATION_PERMISSIONS)
+        }
+
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        Log.d("PAID Maps key is:","${MAPS_API_KEY_PAID}")
 //        TODO: zoom to the user location after taking his permission
 //        TODO: add style to the map
 //        TODO: put a marker to location that the user selected
-
 
 //        TODO: call this function after the user confirms on the selected location
         onLocationSelected()
@@ -46,7 +70,6 @@ class SelectLocationFragment : BaseFragment() {
         //         and navigate back to the previous fragment to save the reminder and add the geofence
     }
 
-
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.map_options, menu)
     }
@@ -54,7 +77,6 @@ class SelectLocationFragment : BaseFragment() {
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         // TODO: Change the map type based on the user's selection.
         R.id.aubergine_map -> {
-
             true
         }
         R.id.gray_map -> {
@@ -69,5 +91,9 @@ class SelectLocationFragment : BaseFragment() {
         else -> super.onOptionsItemSelected(item)
     }
 
+    override fun onMapReady(map: GoogleMap?) {
+        mMap=map
+        mMap?.setMapStyle(MapStyleOptions.loadRawResourceStyle(context, R.raw.gray_map_style))
 
+    }
 }
