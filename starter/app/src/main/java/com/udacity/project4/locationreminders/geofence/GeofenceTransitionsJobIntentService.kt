277d1d5f4aab3b3eff9086c7c882b0996234ffbd
m_wallet_app <mwallet.apps@gmail.com>
2022-11-20 00:28:31 +0200

    SendNotification onHandleWork IntentService GeofenceTransitions

--- starter/app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.JobIntentService
 import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingEvent
 import com.udacity.project4.utils.sendNotification
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
@@ -33,9 +34,13 @@ class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
     }
 
     override fun onHandleWork(intent: Intent) {
-        //TODO: handle the geofencing transition events and
-        // send a notification to the user when he enters the geofence area
-        //TODO call @sendNotification
+
+        val geofencingEvent=GeofencingEvent.fromIntent(intent)
+        if (!geofencingEvent.hasError()){
+            if (geofencingEvent.geofenceTransition==Geofence.GEOFENCE_TRANSITION_ENTER){
+                sendNotification(geofencingEvent.triggeringGeofences)
+            }
+        }
     }
 
     //TODO: get the request id of the current geofence
@@ -64,5 +69,4 @@ class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
             }
         }
     }
-
 }
