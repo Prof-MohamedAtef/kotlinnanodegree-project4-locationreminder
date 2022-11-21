b46142f892eb9f4e2e4003ebcdd21e75f881a64a
m_wallet_app <mwallet.apps@gmail.com>
2022-11-21 15:08:46 +0200

    added a FakeTest Double as FakeDataSource.kt

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -1,28 +1,53 @@
 package com.udacity.project4.locationreminders.data
 
+import androidx.lifecycle.MutableLiveData
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var remindersList: MutableList<ReminderDTO>? = mutableListOf()) :
+    ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    var remindersServiceData: LinkedHashMap<String, ReminderDTO> = LinkedHashMap()
+    private val observableReminders = MutableLiveData<Result<List<ReminderDTO>>>()
+
+    private var shouldReturnError = false
+
+    fun setReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (shouldReturnError) {
+            return Result.Error("testing error!")
+        }
+        remindersList?.let {
+            return Result.Success(ArrayList(it))
+        }
+        return Result.Error("not found!")
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        remindersList?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        if (shouldReturnError) {
+            return Result.Error("testing error!")
+        }
+        remindersList?.let {
+            return Result.Success(it.single { reminder -> reminder.id == id })
+        }
+        return Result.Error("not found!")
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        remindersList?.clear()
     }
 
-
+    fun addReminder(vararg reminders: ReminderDTO) {
+        for (reminder in reminders) {
+            remindersServiceData[reminder.id] = reminder
+        }
+    }
 }
--- starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -3,6 +3,7 @@ package com.udacity.project4.locationreminders.reminderslist
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.MainCoroutineDispatcher
 import org.junit.Rule
@@ -23,4 +24,6 @@ class RemindersListViewModelTest {
     var mainCoroutineDispatcherRule = MainCoroutineRule()
 
 
+    // use test double/Fake data source to be injected into the viewmodel
+    private lateinit var dataSource: FakeDataSource
 }
